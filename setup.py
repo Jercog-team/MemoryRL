b3558f3c3bbdfc76f85f2e7ce70ab612b5bac52b
Gonzalo <46898681+gonzalohooh@users.noreply.github.com>
2025-01-16 13:29:41 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mouse_behavior_pomdp",
+    version="0.1.0",
+    description="A POMDP-based simulation of mouse behavior in multi-port experiments.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jercog-team/MemoryRL",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'run_mouse_pomdp=main:main'
+        ],
+    },
+)
