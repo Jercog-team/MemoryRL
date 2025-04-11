c2796f61b435e7949cfaad9748e1c536b9bbf187
Gonzalo <46898681+gonzalohooh@users.noreply.github.com>
2025-04-11 10:37:13 +0200

    readme

--- memoryrl/pomdp/__init__.py
+++ memoryrl/pomdp/__init__.py
@@ -1,13 +1,3 @@
-"""
-POMDP Module
-
-This module contains the implementation of Partially Observable Markov Decision Process (POMDP) logic.
-
-Submodules:
-- belief.py: Belief state management.
-- policy.py: Policy-related functions.
-"""
-
 from .belief import initialize_belief_von_misses, initialize_belief_deltas, update_belief
 from .policy import greedy_policy_marginal_belief, greedy_policy_last_visit, bellman_policy
 
--- memoryrl/visualization/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Visualization Module
-
-This module contains functions for visualizing data and results in the MemoryRL framework.
-
-Submodules:
-- animation.py: Animation-related utilities.
-- plotting.py: Plotting-related utilities.
-"""
