f5b6e8979ebbe59621468fb5576f2444194f045c
Gonzalo <46898681+gonzalohooh@users.noreply.github.com>
2025-11-16 11:09:25 +0100

    Delete memoryrl/utils directory

--- memoryrl/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_processing import (convert_to_angular_poked, rad_avg_poked, hist_data_distance, circular_distance, water_availability, subsample_data, MemoryIndex_histogram, figure_data_based_MI, figure_model_based_MI, train_hmm_models, model_based_MI, distance_surrogates)
-
-
-__all__ = ["convert_to_angular_poked", "rad_avg_poked", "hist_data_distance", "circular_distance", "water_availability", "subsample_data", "MemoryIndex_histogram", "figure_data_based_MI", "figure_model_based_MI", "train_hmm_models", "model_based_MI", "distance_surrogates"]
